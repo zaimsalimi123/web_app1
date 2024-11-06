aff9bc53848abec7d453cb0cd343c81bceeacffc
zaimsalimi123 <130406225+zaimsalimi123@users.noreply.github.com>
2024-11-06 18:55:23 +0500

    Update webapp.py
    made changes for duplicate items/ todos.

--- webapp.py
+++ webapp.py
@@ -2,25 +2,23 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
-
 def add_todo():
     todo = st.session_state["new_todo"] + '\n'
     todos.append(todo)
     functions.write_todos(todos)
 
 st.title("My Todo App")
-st.subheader("This app is to improve your productivity.")
-st.write("This is my todo app")
+st.subheader("This App improves your Creativity")
+st.write("Lets add some todos.")
+st.text_input(label="Write your todos.", placeholder="Enter Todo",
+              key="new_todo", on_change=add_todo)
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo , key=todo)
+    checkbox = st.checkbox(todo, key=f"{todo}_{index}")
+
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todo]
+        del st.session_state[f"{todo}_{index}"]
         st.rerun()
 
-st.text_input(label="Enter todo", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo')
-
-st.session_state
